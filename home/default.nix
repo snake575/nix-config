bc7d80a1d8015b6fc9198abc598b9f3f1c393b1f
Sebastian Aranguiz <sarang575@gmail.com>
2025-08-30 03:18:52 -0400

    Update flake.lock and home.stateVersion

--- home/default.nix
+++ home/default.nix
@@ -96,5 +96,5 @@
   #
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   # https://nixos.org/manual/nixos/unstable/release-notes.html
-  home.stateVersion = "24.11";
+  home.stateVersion = "25.11";
 }
