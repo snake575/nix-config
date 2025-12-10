77ee9e46facabe7172135651be280390b85e5983
Sebastian Aranguiz <sarang575@gmail.com>
2025-12-10 15:35:05 -0300

    Add nh application configuration to home manager and include in default.nix

---
+++ home/apps/nh.nix
@@ -0,0 +1,9 @@
+{
+  programs.nh = {
+    enable = false; # TODO: disabled pending nh fix for WSL PATH spaces (see issue https://github.com/nix-community/nh/issues/421)
+    # keep config ready to re-enable once upstream fixes sudo/env PATH handling
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 7d --keep 3";
+    homeFlake = "/home/snake575/.config/home-manager";
+  };
+}
--- home/default.nix
+++ home/default.nix
@@ -11,6 +11,7 @@
     ./apps/btop.nix
     ./apps/lsd.nix
     ./apps/poetry.nix
+    ./apps/nh.nix
   ];
 
   # Let Home Manager install and manage itself.
