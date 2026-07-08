db249b55232ac5ec603983f1451a5adeb1d8b9dd
Sebastian Aranguiz <sarang575@gmail.com>
2026-07-08 12:09:04 -0400

    Update home-manager flake

--- home/apps/nh.nix
+++ home/apps/nh.nix
@@ -3,8 +3,7 @@
 {
   # nh: nix helper CLI
   programs.nh = {
-    enable = false; # TODO: disabled pending nh fix for WSL PATH spaces (see issue https://github.com/nix-community/nh/issues/421)
-    # keep config ready to re-enable once upstream fixes sudo/env PATH handling
+    enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 7d --keep 3";
     homeFlake = "${config.home.homeDirectory}/.config/home-manager";
