2d647641e48adbe8850df53d1248a62b2d40d1ec
Sebastian Aranguiz <sarang575@gmail.com>
2026-07-16 21:49:49 -0400

    Disable GPU driver integration for headless VM

--- home/linux.nix
+++ home/linux.nix
@@ -4,6 +4,8 @@
   imports = [ ./default.nix ];
 
   targets.genericLinux.enable = true;
+  # This VM is permanently headless, so it does not need host GPU driver integration.
+  targets.genericLinux.gpu.enable = false;
 
   home.sessionPath = [
     "/home/linuxbrew/.linuxbrew/bin"
